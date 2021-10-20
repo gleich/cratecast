ed42677c72b11441534450acaf06d07b06f536fb
Matt Gleich <git@mattglei.ch>
2021-10-19 22:33:54 -0400

    feat: add prettier
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,31 @@
-import { ActionPanel, CopyToClipboardAction, Icon, List, OpenInBrowserAction, render } from "@raycast/api";
-import { randomUUID } from "crypto";
-import { useState } from "react";
-import { Crate, getCrates } from "./api";
-import { useDebouncedCallback } from "use-debounce";
+import {
+  ActionPanel,
+  CopyToClipboardAction,
+  Icon,
+  List,
+  OpenInBrowserAction,
+  render,
+} from '@raycast/api'
+import { randomUUID } from 'crypto'
+import { useState } from 'react'
+import { Crate, getCrates } from './api'
+import { useDebouncedCallback } from 'use-debounce'
 
-render(<Main />);
+render(<Main />)
 
 function Main(): JSX.Element {
-  const [crates, setCrates] = useState<Crate[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [crates, setCrates] = useState<Crate[]>([])
+  const [loading, setLoading] = useState(false)
   const debounced = useDebouncedCallback(async (v) => {
-    setLoading(true);
-    setCrates(await getCrates(v));
-    setLoading(false);
-  }, 500);
+    setLoading(true)
+    setCrates(await getCrates(v))
+    setLoading(false)
+  }, 500)
 
   return (
     <List isLoading={loading} onSearchTextChange={(v) => debounced(v)}>
       {crates.map((c) => {
-        const id = c.name + randomUUID();
+        const id = c.name + randomUUID()
         return (
           <List.Item
             id={id}
@@ -29,19 +36,39 @@ function Main(): JSX.Element {
             accessoryIcon={Icon.Download}
             actions={
               <ActionPanel>
-                <CopyToClipboardAction content={`${c.name} = "${c.version}"`} title="Copy Dependency Line" />
+                <CopyToClipboardAction
+                  content={`${c.name} = "${c.version}"`}
+                  title="Copy Dependency Line"
+                />
                 {c.documentationURL ? (
-                  <OpenInBrowserAction url={c.documentationURL} title="Open Crate Documentation" />
+                  <OpenInBrowserAction
+                    url={c.documentationURL}
+                    title="Open Crate Documentation"
+                  />
                 ) : (
                   <></>
                 )}
-                {c.homepageURL ? <OpenInBrowserAction url={c.homepageURL} title="Open Homepage" /> : <></>}
-                {c.repositoryURL ? <OpenInBrowserAction url={c.repositoryURL} title="Open Repository" /> : <></>}
+                {c.homepageURL ? (
+                  <OpenInBrowserAction
+                    url={c.homepageURL}
+                    title="Open Homepage"
+                  />
+                ) : (
+                  <></>
+                )}
+                {c.repositoryURL ? (
+                  <OpenInBrowserAction
+                    url={c.repositoryURL}
+                    title="Open Repository"
+                  />
+                ) : (
+                  <></>
+                )}
               </ActionPanel>
             }
           />
-        );
+        )
       })}
     </List>
-  );
+  )
 }
